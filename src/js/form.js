492e7e49e359f3861a670920ea974e765bb3328a
Pavel Proyavin <pavel@dorohovdesign.com>
2019-05-27 10:18:58 +0300

    Added modal window and anchor

--- src/js/form.js
+++ src/js/form.js
@@ -3,6 +3,12 @@
     $(function() {
 
         $('form').parsley()
+        $('#modal_form').modal()
+        $('.anchor').on('click', function(e) {
+            e.preventDefault()
+            var aid = $(this).attr("href");
+            $('html,body').animate({scrollTop: $(aid).offset().top - $('.navbar').innerHeight()},'slow');
+        })
 
     })
 })(jQuery);
